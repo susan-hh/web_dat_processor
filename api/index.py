a78ad986035bb7d433375e5bfa0829527120f5c4
susan-hh <ailijia365@hotmail.com>
2024-12-07 00:53:53 +0800

    Add favicon and healthcheck support

--- api/index.py
+++ api/index.py
@@ -1,5 +1,17 @@
-from app import app
+from flask import Flask, send_from_directory
+from app import app as flask_app
+import os
 
-# Vercel serverless function handler
-def handler(request, response):
-    return app(request, response) 
+app = flask_app
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, 'static'),
+        'favicon.ico', 
+        mimetype='image/vnd.microsoft.icon'
+    )
+
+@app.route('/api/healthcheck')
+def healthcheck():
+    return {"status": "ok"}
